559f475fc515d3483188a2fdd3cb6115ca6ca119
Stanislaw Horna <stanislawhorna@outlook.com>
2024-11-24 18:17:35 +0000

    update init sh

--- init-db.sh
+++ init-db.sh
@@ -5,3 +5,8 @@ for file in /data/*.json; do
     collection=$(basename "$file" .json)
     mongoimport --host localhost --port 27017 --db THDC --collection "$collection" --file "$file" --jsonArray
 done
+
+for file in /Secrets/*.json; do
+    collection=$(basename "$file" .json)
+    mongoimport --host localhost --port 27017 --db THDC --collection "Secrets" --file "$file" --jsonArray
+done
